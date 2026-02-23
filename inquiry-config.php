82f8517fb5c37db6463b1242bb76f4ae25afc950
supportdcbindia <120620782+supportdcbindia@users.noreply.github.com>
2026-02-23 18:08:51 +0530

    Update inquiry-config.php

--- inquiry-config.php
+++ inquiry-config.php
@@ -16,7 +16,7 @@ define('IS_DOMESTIC', true);
 ## EMAIL Settings
 define('FROM_EMAIL', 'info@avalonseparation.com');
 define('TO_EMAIL', 'info@avalonseparation.com');
-define('CC_EMAIL', '');
+define('CC_EMAIL', 'avalonseparation@gmail.com');
 define('EMAIL_SUBJECT', 'New Inquiry From '.COMPANY_NAME.' Website');
 define('CATALOGUE_EMAIL_SUBJECT', 'New Catalogue Request From '.COMPANY_NAME.' Website');
 ## SMTP2GO Username & Password
